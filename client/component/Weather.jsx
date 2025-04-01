f0f35c1baa3913a7a18122e746725f8eb14c25fc
Parn Barve <parnbarve@gmail.com>
2025-04-01 15:57:08 +0530

    env and file structure changes

--- client/component/Weather.jsx
+++ client/component/Weather.jsx
@@ -2,65 +2,67 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "./Weather.css";
 
-function Weather({latitude, longitude}) {
+function Weather({ latitude, longitude }) {
+	const [data, setData] = useState(null);
 
-    const [data, setData] = useState(null);
+	let description = "";
 
-    let description = "";
+	const baseUrl = import.meta.env.VITE_BASEURL + latitude + "/" + longitude;
 
-    const baseUrl = import.meta.env.VITE_BASEURL + latitude + "/" + longitude;
+	useEffect(() => {
+		axios
+			.get(baseUrl)
+			.then((response) => {
+				setData(response.data);
+			})
+			.catch((error) => {
+				console.log(error.response.data);
+			});
+	}, []);
+	// let description = data.current.weather[0].description;
+	// const newDes = description.map(word => {word.charAt(0).toUpperCase() + word.slice(1)})
 
-    useEffect(() => {
-        axios.get(baseUrl)
-        .then(response => {
-            setData(response.data)
-        }).catch((error) => {
-            console.log(error.response.data);
-        })
-    }, []);
-    // let description = data.current.weather[0].description;
-    // const newDes = description.map(word => {word.charAt(0).toUpperCase() + word.slice(1)})
-
-    return(
-        <>
-            {
-                data && 
-                // <p>{JSON.stringify(data)}</p>
-                <>
-                    <div className="left-items weather ">
-                        <div className="left">
-                            <img 
-                                src={`https://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`} 
-                                alt="weather description image"
-                            />
-                            <p id="description">{data.current.weather[0].description}</p>
-                            <p>{data.timezone}</p>
-                        </div>
-                        <div className="right">
-                            <p id="number">{data.current.temp}</p>
-                            <p>Feels Like {data.current.feels_like}</p>
-                        </div>
-                    </div>
-                    <div className="small weather">
-                        <div className="center-items">
-                            <p>Pressure</p>
-                            <p>Humidity</p>
-                            <p>Clouds</p>
-                            <p>Visibility</p>
-                            <p>Wind Speed</p>
-                        </div>
-                        <div className="right-items">
-                            <p>{data.current.pressure}</p>
-                            <p>{data.current.humidity}</p>
-                            <p>{data.current.clouds}</p>
-                            <p>{data.current.visibility}</p>
-                            <p>{data.current.wind_speed}</p>
-                        </div>
-                    </div>
-                </> 
-            }
-        </>
-    );
-};
+	return (
+		<>
+			{data && (
+				// <p>{JSON.stringify(data)}</p>
+				<>
+					<div className="left-items weather ">
+						<div className="left">
+							<img
+								src={`https://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`}
+								alt="weather description image"
+							/>
+							<p id="description">
+								{data.current.weather[0].description}
+							</p>
+							<p>{data.timezone}</p>
+						</div>
+						<div className="right">
+							<p id="number">{data.current.temp}</p>
+							<p>Feels Like {data.current.feels_like}</p>
+						</div>
+					</div>
+					<div className="small weather">
+						<div className="center-items">
+							<p>Pressure</p>
+							<p>Humidity</p>
+							<p>Clouds</p>
+							<p>Visibility</p>
+							<p>Wind Speed</p>
+						</div>
+						<div className="right-items">
+							<p>{data.current.pressure}</p>
+							<p>{data.current.humidity}</p>
+							<p>{data.current.clouds}</p>
+							<p>{data.current.visibility}</p>
+							<p>{data.current.wind_speed}</p>
+						</div>
+					</div>
+				</>
+			)}
+		</>
+	);
+}
 
 export default Weather;
---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import bodyParser from "body-parser";
+// import "dotenv/config";
+import dotenv from "dotenv";
+import axios from "axios";
+import cors from "cors";
+
+dotenv.config({ path: "../.env" });
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+});
+
+app.get("/:lat/:lon", (req, res) => {
+	const lat = req.params.lat;
+	const lon = req.params.lon;
+	const units = "metric";
+	const apiKey = process.env.SECRET_KEY;
+	const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily&appid=${apiKey}&units=${units}`;
+	// console.log(lat, lon);
+	// console.log(apiKey);
+	// console.log(url);
+	axios
+		.get(url)
+		.then((response) => {
+			res.send(response.data);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+});
